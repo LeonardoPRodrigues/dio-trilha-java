3e9b26c346cfd9d7697f13c08918c2ff86d336d2
Leonardo Rodrigues <pereiraleonardo898@gmail.com>
2024-05-03 21:03:30 -0300

    Adicionando o desafio de controle de fluxo

---
+++ DesafioControleFluxo/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import exceptions.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = sc.nextInt();
+        System.out.print("Digite o segundo parâmetro: ");
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro: ");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+            if (parametroUm < parametroDois){
+                int contagem = parametroDois - parametroUm;
+                for (int i = 0; i <= contagem; i++){
+                    System.out.println("Imprimindo o numero: " + (i+1));
+                }
+            }
+            else {
+              throw new ParametrosInvalidosException();
+            }
+    }
+}
